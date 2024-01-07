6ba1b7b043ba69cafee2a103c686eee03038ed09
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-07 13:46:15 -0800

    firebase athentication 1

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBEB4NvfoiTaj5Uwsj0V1SGxlituoBFIdA",
-  authDomain: "netflix-vite-61fd7.firebaseapp.com",
-  projectId: "netflix-vite-61fd7",
-  storageBucket: "netflix-vite-61fd7.appspot.com",
-  messagingSenderId: "269797042193",
-  appId: "1:269797042193:web:89cfbb4cf5514b061ea9b0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
