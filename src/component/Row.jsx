f0f9e762123538cb7ab3eae5054d767112b95fa1
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-07 13:08:06 -0800

    left and right icons are added to the rows with style

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import Movie from './Movie';
 
 const Row = ({title, fetchURL}) => {
@@ -19,12 +20,14 @@ const Row = ({title, fetchURL}) => {
   return (
     <>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-        <div className='relative flex items-center'>
+        <div className='relative flex items-center group'>
+            <MdChevronLeft size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' />
             <div id={'Slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                 {movies.map((item, id) => (
                     <Movie key={id} item={item}/>
                 ))}
             </div>
+            <MdChevronRight size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' />
         </div>
     </>
   )
