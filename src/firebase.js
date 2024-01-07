ba544b7ecf5331192ce601d3769a6f94bbb73964
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-07 13:33:38 -0800

    firebase is made

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEB4NvfoiTaj5Uwsj0V1SGxlituoBFIdA",
+  authDomain: "netflix-vite-61fd7.firebaseapp.com",
+  projectId: "netflix-vite-61fd7",
+  storageBucket: "netflix-vite-61fd7.appspot.com",
+  messagingSenderId: "269797042193",
+  appId: "1:269797042193:web:89cfbb4cf5514b061ea9b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
