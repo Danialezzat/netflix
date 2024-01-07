2489d2e0e442a20b9e03f71d42256ef64110ff91
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-07 12:06:59 -0800

    Row component is made and is added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from '../component/Main'
+import Row from '../component/Row'
 
 const Home = () => {
   return (
     <div>
         <Main />
+        <Row />
     </div>
   )
 }
