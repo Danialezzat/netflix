fafcf559a45fe52f75987ef504ee37f46569b2d4
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-08 00:36:09 -0800

    athentication folder is amde and the context is added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AthContext";
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      </AuthContextProvider>
     </>
   );
 }
