364c7aeb6cc15356775911fa7f50199cd6e63bbf
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-08 01:15:46 -0800

    background image is added to the signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <>
+      <div className='w-full h-screen '>
+        <img className='hidden sm:block absolute w-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/94c5161a-3975-4977-80d9-158b78980cf2/IT-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+      </div>
+    </>
   )
 }
 
