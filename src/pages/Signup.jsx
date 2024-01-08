4723b3ccb16003ba94316ccd8b1f30d77223a3fd
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-08 01:41:04 -0800

    below help made and background is full

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
     <>
-      <div className='w-full h-screen '>
-        <img className='hidden sm:block absolute w-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/94c5161a-3975-4977-80d9-158b78980cf2/IT-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
-        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
+      <div className='w-full h-full '>
+        <img className='hidden sm:block absolute w-full sm:h-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/94c5161a-3975-4977-80d9-158b78980cf2/IT-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-full'></div>
         <div className='fixed w-full px-4 py-24 z-50'>
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
@@ -14,10 +15,21 @@ const Signup = () => {
                 <input className='p-3 my-2 bg-gray-700 rounded' type="email"  placeholder='email...' autoComplete='email'/>
                 <input className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='password...' autoComplete='current-password'/>
                 <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
+
+
                 <div className='flex justify-between items-center text-sm text-gray-600'>
                   <p><input className='mr-2' type="checkbox"/>Remember Me</p>
                   <p>Need Help?</p>
                 </div>
+
+                <p className='py-8'>
+                  <span className='text-gray-600'>
+                    Already subscribed to Netflix?
+                  </span>{' '}
+                  <Link to='/signin'>
+                    Sign In
+                  </Link>
+                </p>
               </form>
             </div>
           </div>
