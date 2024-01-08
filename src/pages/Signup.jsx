fe529d707f0b51954d9357fd20bf94d4a1c92f68
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-08 01:59:54 -0800

    onsubmit is added to the form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,7 +28,9 @@ const Signup = () => {
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
-              <form className='w-full flex flex-col py-4'>
+              <form 
+               onSubmit={handleSubmit}
+               className='w-full flex flex-col py-4'>
                 <input className='p-3 my-2 bg-gray-700 rounded' type="email"  placeholder='email...' autoComplete='email'/>
                 <input className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='password...' autoComplete='current-password'/>
                 <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
