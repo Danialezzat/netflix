3281ee96ced77a5f3e0f10c8754e9d16e5c79964
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-08 13:17:23 -0800

    saved shows structure is made

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <>
+      <div className='w-full text-white'>
+      <img className=' w-full h-[400px] object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/94c5161a-3975-4977-80d9-158b78980cf2/IT-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+      <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+      <div className='absolute top-[20%] p-4 md:p-8'>
+        <h1 className='text-3xl md:text-5xl font-bold '>My Shows</h1>
+      </div>
+      </div>
+    </>
   )
 }
 
